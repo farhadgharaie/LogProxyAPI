ff4e558224dd77539b3ce31e6e8f863b57163e99
Farhad <f.gharaie@gmail.com>
2021-10-17 15:57:46 +0330

    Add basic authentication handler

---
+++ Helper/BasicAuthenticationHandler.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace LogProxyAPI.Helper
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private IConfiguration _configuration;
+        public BasicAuthenticationHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock,
+        IConfiguration configuration
+        )
+           : base(options, logger, encoder, clock)
+        {
+            _configuration = configuration;
+        }
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Invalid Authorization Header"));
+            }
+            try
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':');
+                var username = credentials[0];
+                var password = credentials[1];
+
+                if (_configuration[username] != username || _configuration[password] != password)
+                {
+                    return Task.FromResult(AuthenticateResult.Fail("Invalid Credential")); ;
+                }
+
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier,username)
+                };
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var claimsPrincipal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(claimsPrincipal, Scheme.Name);
+                return Task.FromResult(AuthenticateResult.Success(ticket));
+            }
+            catch
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Invalid Authorization Header"));
+            }
+        }
+    }
+}
